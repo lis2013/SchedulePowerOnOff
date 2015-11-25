78d19064ae1d2d439155431f26f5f91b58249d6c
chenxy <chenxy@polyvi.com>
2015-11-25 16:37:14 +0800

    Regulate the layout of time set activity, modify some image resources

--- src/com/borqs/schedulepoweronoff/TimeSetActivity.java
+++ src/com/borqs/schedulepoweronoff/TimeSetActivity.java
@@ -26,81 +26,81 @@ import com.borqs.schedulepoweronoff.alarmdatastorage.AlarmModel;
 import com.borqs.schedulepoweronoff.utils.AlarmUtils;
 
 public class TimeSetActivity extends Activity implements TimePickerDialog.OnTimeSetListener, OnClickListener {
-	private ListView mList;
-	private int mTimeType;
-	private ImageButton mResetButton;
-	private ImageButton mFinishButton;
-	private AlarmModel mAlarmModel;
+    private ListView mList;
+    private int mTimeType;
+    private ImageButton mResetButton;
+    private ImageButton mFinishButton;
+    private AlarmModel mAlarmModel;
     private final static int REPEAT_DIALOG = 1;
     boolean[] checked = new boolean[] { false, false, false, false, false, false, false };
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.time_set_layout);
-		mAlarmModel = AlarmModel.convertToObj(getIntent().getExtras()
-				.getString(AlarmModel.ENTITY));
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time_set_layout);
+        mAlarmModel = AlarmModel.convertToObj(getIntent().getExtras()
+                .getString(AlarmModel.ENTITY));
 
-		mResetButton = (ImageButton) findViewById(R.id.reset);
-		mFinishButton = (ImageButton) findViewById(R.id.finish);
-		mResetButton.setOnClickListener(this);
-		mFinishButton.setOnClickListener(this);
-		if (mTimeType == AlarmEntity.POWERON_CLOCK) {
-			setTitle(R.string.set_power_on);
-		} else {
-			setTitle(R.string.set_power_off);
-		}
-		mList = (ListView) findViewById(android.R.id.list);
-		updateListView();
-	}
+        mResetButton = (ImageButton) findViewById(R.id.reset);
+        mFinishButton = (ImageButton) findViewById(R.id.finish);
+        mResetButton.setOnClickListener(this);
+        mFinishButton.setOnClickListener(this);
+        if (mTimeType == AlarmEntity.POWERON_CLOCK) {
+            setTitle(R.string.set_power_on);
+        } else {
+            setTitle(R.string.set_power_off);
+        }
+        mList = (ListView) findViewById(android.R.id.list);
+        updateListView();
+    }
 
-	private void updateListView() {
-		SimpleAdapter adapter = getListAdapter();
-		mList.setAdapter(adapter);
-		mList.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				switch (position) {
-				case 0:
-					new TimePickerDialog(
-							TimeSetActivity.this,
-							TimeSetActivity.this,
-							mAlarmModel.getEntity().getHour(),
-							mAlarmModel.getEntity().getMinute(),
-							DateFormat
-									.is24HourFormat(TimeSetActivity.this))
-							.show();
-					break;
-				case 1:
+    private void updateListView() {
+        SimpleAdapter adapter = getListAdapter();
+        mList.setAdapter(adapter);
+        mList.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                switch (position) {
+                case 0:
+                    new TimePickerDialog(
+                            TimeSetActivity.this,
+                            TimeSetActivity.this,
+                            mAlarmModel.getEntity().getHour(),
+                            mAlarmModel.getEntity().getMinute(),
+                            DateFormat
+                                    .is24HourFormat(TimeSetActivity.this))
+                            .show();
+                    break;
+                case 1:
 					showDialog(REPEAT_DIALOG);
-					break;
-				}
-			}
-		});
-	}
+                    break;
+                }
+            }
+        });
+    }
 
-	private SimpleAdapter getListAdapter() {
-		String[] title = { this.getResources().getString(R.string.time_text),
-				this.getResources().getString(R.string.repeat) };
-		String[] info = { mAlarmModel.getTime(), mAlarmModel.getRepeatedStr(this) };
-		int[] imageids = { R.drawable.show_icon_time,
-				R.drawable.show_icon_repeat };
-		List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < title.length; i++) {
-			Map<String, Object> listem = new HashMap<String, Object>();
-			listem.put("title", title[i]);
-			listem.put("info", info[i]);
-			listem.put("imageid", imageids[i]);
-			listems.add(listem);
-		}
+    private SimpleAdapter getListAdapter() {
+        String[] title = { this.getResources().getString(R.string.time_text),
+                this.getResources().getString(R.string.repeat) };
+        String[] info = { mAlarmModel.getTime(), mAlarmModel.getRepeatedStr(this) };
+        int[] imageids = { R.drawable.next,
+                R.drawable.next };
+        List<Map<String, Object>> listems = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < title.length; i++) {
+            Map<String, Object> listem = new HashMap<String, Object>();
+            listem.put("title", title[i]);
+            listem.put("info", info[i]);
+            listem.put("imageid", imageids[i]);
+            listems.add(listem);
+        }
 
-		SimpleAdapter simpleAdapter = new SimpleAdapter(this, listems,
-				R.layout.time_set_list_item, new String[] { "title", "info",
-						"imageid" }, new int[] { R.id.title_text,
-						R.id.info_text, R.id.show_button }) ;
-		return simpleAdapter;
-	}
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, listems,
+                R.layout.time_set_list_item, new String[] { "title", "info",
+                        "imageid" }, new int[] { R.id.title_text,
+                        R.id.info_text, R.id.show_button }) ;
+        return simpleAdapter;
+    }
 
     @Override
     protected Dialog onCreateDialog(int id) {
@@ -134,31 +134,31 @@ public class TimeSetActivity extends Activity implements TimePickerDialog.OnTime
         return dialog;
     }
 
-	@Override
+    @Override
 	public void onResume() {
 		super.onResume();
-	}
+    }
 
-	@Override
-	public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-		mAlarmModel.setTime(this, hourOfDay, minute);
-		updateListView();
-	}
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mAlarmModel.setTime(this, hourOfDay, minute);
+        updateListView();
+    }
 
-	@Override
-	public void onClick(View v) {
-		int id = v.getId();
-		switch (id) {
-		case R.id.reset:
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+        case R.id.reset:
+            finish();
+            break;
+        case R.id.finish:
+            mAlarmModel.enable(this, mAlarmModel.isEnabled());
+            AlarmUtils.toastAlarmPeriod(this, mAlarmModel);
 			finish();
-			break;
-		case R.id.finish:
-			mAlarmModel.enable(this, mAlarmModel.isEnabled());
-			AlarmUtils.toastAlarmPeriod(this, mAlarmModel);
-			finish();
-			break;
-		}
+            break;
+        }
 
-	}
+    }
 
 }
