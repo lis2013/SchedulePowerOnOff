7ffc654fc7dc514e103d1e3b4285fc58954a8544
chenxy <chenxy@polyvi.com>
2015-11-25 12:44:31 +0800

    Add local jar lib for make file

--- Android.mk
+++ Android.mk
@@ -2,6 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_JAVA_LIBRARIES := bouncycastle
+LOCAL_STATIC_JAVA_LIBRARIES := gson
 
 LOCAL_MODULE_TAGS := optional
 
@@ -11,6 +12,9 @@ LOCAL_PACKAGE_NAME := SchedulePowerOnOff
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := gson:libs/gson-2.3.1.jar
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
